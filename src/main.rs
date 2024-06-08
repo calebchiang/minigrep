3a0877a29daffd77bb047ec5e9841daabe7d125c
Caleb Chiang <calebchiang1002@gmail.com>
2024-06-08 13:01:43 -0700

    Seperated program logic from main

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,21 @@
 use std::env;
-use std::fs;
 use std::process;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result <Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        Ok(Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
-        })
-    }
-}
-
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("{}", contents);
-}
+use minigrep;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // Read any command line arguments and collect the values into a vector
+
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    run(config);
+    if let Err(e) = minigrep::run(config) { // Use if let and not unwrap because there is no value to be returned in Ok instance
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 
 }
